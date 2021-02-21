2ffaf851da20cb61bed558c290432391f198d2f9
JeganS948 <69284232+JeganS948@users.noreply.github.com>
2021-02-20 19:54:11 -0500

    Update logic2.js

--- leaflet_step_2/static/js/logic2.js
+++ leaflet_step_2/static/js/logic2.js
@@ -1,5 +1,6 @@
 // Store our API endpoint inside queryUrl
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+var platesUrl = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json"
 
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
@@ -72,10 +73,28 @@ function createMap(earthquakes) {
     accessToken: API_KEY
   });
 
+  var outdoorsmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/outdoors-v11",
+    accessToken: API_KEY
+  });
+
+  var satellitemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.satellite",
+    accessToken: API_KEY
+  });
+
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
     //"Street Map": streetmap,
-    "Light Map": lightmap
+    "Light Map": lightmap,
+    "Outdoor Map": outdoorsmap,
+    "Satellite Map": satellitemap
   };
 
   // Create overlay object to hold our overlay layer
