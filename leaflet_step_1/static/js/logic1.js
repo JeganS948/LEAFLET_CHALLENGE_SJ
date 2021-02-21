4e6feaa4ef4d032a0b34322ad49449f95018f5e0
JeganS948 <69284232+JeganS948@users.noreply.github.com>
2021-02-20 19:22:07 -0500

    Update logic1.js

--- leaflet_step_1/static/js/logic1.js
+++ leaflet_step_1/static/js/logic1.js
@@ -1,35 +1,119 @@
-// Retrieve data set as a URL of JSON file
-var alleqUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+// Store our API endpoint inside queryUrl
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// Initialize & Create Two Separate LayerGroups: earthquakes & tectonicPlates
-var earthquakes = new L.LayerGroup();
-
-// Perform a GET request to query URL
-d3.json(alleqUrl, function(data) {
-    // Send data response to data.features function object to the createFeatures function
-    //createFeatures(data.features);
-    console.log(data.features)
+// Perform a GET request to the query URL
+d3.json(queryUrl, function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+  console.log(data)
+  createFeatures(data.features);
+  
 });
 
-// Create Map
-// Define variables for tile layers
-var grayscaleMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+function createFeatures(earthquakeData) {
+
+  // Define a function we want to run once for each feature in the features array
+  // Give each feature a popup describing the place and time of the earthquake
+  function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.place +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+  }
+    // Define function to create the circle radius based on the magnitude
+    function radiusSize(magnitude) {
+        return magnitude * 20000;
+    }
+
+   // Define function to set the circle color based on the magnitude
+   function circleColor(magnitude) {
+    if (magnitude < 1) {
+      return "#ccff33"
+    }
+    else if (magnitude < 2) {
+      return "#ffff33"
+    }
+    else if (magnitude < 3) {
+      return "#ffcc33"
+    }
+    else if (magnitude < 4) {
+      return "#ff9933"
+    }
+    else if (magnitude < 5) {
+      return "#ff6633"
+    }
+    else {
+      return "#ff3333"
+    }
+  }
+
+  // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
+  var earthquakes = L.geoJSON(earthquakeData, {
+    pointToLayer: function(earthquakeData, latlng) {
+      return L.circle(latlng, {
+        radius: radiusSize(earthquakeData.properties.mag),
+        color: circleColor(earthquakeData.properties.mag),
+        fillOpacity: 1
+      });
+    },
+    onEachFeature: onEachFeature
+  });
+
+  // Sending our earthquakes layer to the createMap function
+  createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+
+  // Define streetmap and darkmap layers
+  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
-    id: "mapbox/light-v10",
+    id: "light-v10",
     accessToken: API_KEY
-});
+  });
 
-// Define baseMaps object to hold our base layers
-var baseMaps = {
-    "Grayscale": grayscaleMap
-};
+  // Define a baseMaps object to hold our base layers
+  var baseMaps = {
+    //"Street Map": streetmap,
+    "Light Map": lightmap
+  };
 
-// Create our map, giving it the streetmap and earthquakes layers to display on load
-var myMap = L.map("mapid", {
+  // Create overlay object to hold our overlay layer
+  var overlayMaps = {
+    Earthquakes: earthquakes
+  };
+
+  // Create our map, giving it the streetmap and earthquakes layers to display on load
+  var myMap = L.map("mapid", {
     center: [
-        37.09, -95.71
+      37.09, -95.71
     ],
     zoom: 5,
-    layers: [grayscaleMap]
-});
+    layers: [lightmap, earthquakes]
+  });
+
+  // Create a layer control
+  // Pass in our baseMaps and overlayMaps
+  // Add the layer control to the map
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+}
+
+// Set Up Legend
+var legend = L.control({ position: "bottomright" })
+legend.onAdd = function() {
+    var div = DomUtil.create("div", "info legend"),
+    magnitudeLevels = [0, 1, 2, 3, 4, 5];
+
+    div.innerHTML += "<h3>Magnitude</h3>"
+ 
+         for (var i = 0; i < magnitudeLevels.length; i++) {
+             div.innerHTML +=
+                 '<i style="background: ' + chooseColor(magnitudeLevels[i] + 1) + '"></i> ' +
+                 magnitudeLevels[i] + (magnitudeLevels[i + 1] ? '&ndash;' + magnitudeLevels[i + 1] + '<br>' : '+');
+         }
+         return div;
+};
+
+// Add Legend to the Map
+legend.addTo(myMap);
