73290959f06a0a6990b01fa59b1fced314dcd20f
JeganS948 <69284232+JeganS948@users.noreply.github.com>
2021-02-20 19:38:29 -0500

    config

--- leaflet_step_2/static/js/config2.js
+++ leaflet_step_2/static/js/config2.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoiamVnYW40IiwiYSI6ImNrbGVkcDhzMTRqcWQyb3Bsb2dhYzRvZmcifQ.eMB2T97Xlr93qmMGpd7W6g";
+const API_KEY = "pk.eyJ1IjoiamVnYW40IiwiYSI6ImNrbGVmOHQ5bTFqY3UycXA5czdubDd4MjUifQ.DHQo5qeGKVEujwNSBhYoZg";
