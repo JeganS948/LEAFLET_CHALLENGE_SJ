574a03c2f38986e3d3ebe1b0a094a7cc4aeef0f4
JeganS948 <69284232+JeganS948@users.noreply.github.com>
2021-02-20 19:32:42 -0500

    file 1 recon

--- leaflet_step_1/static/js/logic1.js
+++ leaflet_step_1/static/js/logic1.js
@@ -17,33 +17,7 @@ function createFeatures(earthquakeData) {
     layer.bindPopup("<h3>" + feature.properties.place +
       "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
   }
-    // Define function to create the circle radius based on the magnitude
-    function radiusSize(magnitude) {
-        return magnitude * 20000;
-    }
-
-   // Define function to set the circle color based on the magnitude
-   function circleColor(magnitude) {
-    if (magnitude < 1) {
-      return "#ccff33"
-    }
-    else if (magnitude < 2) {
-      return "#ffff33"
-    }
-    else if (magnitude < 3) {
-      return "#ffcc33"
-    }
-    else if (magnitude < 4) {
-      return "#ff9933"
-    }
-    else if (magnitude < 5) {
-      return "#ff6633"
-    }
-    else {
-      return "#ff3333"
-    }
-  }
-
+    
   // Create a GeoJSON layer containing the features array on the earthquakeData object
   // Run the onEachFeature function once for each piece of data in the array
   var earthquakes = L.geoJSON(earthquakeData, {
@@ -59,6 +33,33 @@ function createFeatures(earthquakeData) {
 
   // Sending our earthquakes layer to the createMap function
   createMap(earthquakes);
+}
+
+// Define function to create the circle radius based on the magnitude
+function radiusSize(magnitude) {
+    return magnitude * 20000;
+}
+
+// Define function to set the circle color based on the magnitude
+function circleColor(magnitude) {
+    if (magnitude < 1) {
+    return "#ccff33"
+    }
+    else if (magnitude < 2) {
+    return "#ffff33"
+    }
+    else if (magnitude < 3) {
+    return "#ffcc33"
+    }
+    else if (magnitude < 4) {
+    return "#ff9933"
+    }
+    else if (magnitude < 5) {
+    return "#ff6633"
+    }
+    else {
+    return "#ff3333"
+    }
 }
 
 function createMap(earthquakes) {
@@ -97,23 +98,23 @@ function createMap(earthquakes) {
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
+
+  // Set Up Legend
+    var legend = L.control({ position: "bottomright" })
+    legend.onAdd = function() {
+        var div = L.DomUtil.create("div", "info legend"),
+        magnitudeLevels = [0, 1, 2, 3, 4, 5];
+
+        div.innerHTML += "<h3>Magnitude</h3>"
+    
+            for (var i = 0; i < magnitudeLevels.length; i++) {
+                div.innerHTML +=
+                    '<i style="background: ' + circleColor(magnitudeLevels[i] + 1) + '"></i> ' +
+                    magnitudeLevels[i] + (magnitudeLevels[i + 1] ? '&ndash;' + magnitudeLevels[i + 1] + '<br>' : '+');
+            }
+            return div;
+    };
+
+    // Add Legend to the Map
+    legend.addTo(myMap);
 }
-
-// Set Up Legend
-var legend = L.control({ position: "bottomright" })
-legend.onAdd = function() {
-    var div = DomUtil.create("div", "info legend"),
-    magnitudeLevels = [0, 1, 2, 3, 4, 5];
-
-    div.innerHTML += "<h3>Magnitude</h3>"
- 
-         for (var i = 0; i < magnitudeLevels.length; i++) {
-             div.innerHTML +=
-                 '<i style="background: ' + chooseColor(magnitudeLevels[i] + 1) + '"></i> ' +
-                 magnitudeLevels[i] + (magnitudeLevels[i + 1] ? '&ndash;' + magnitudeLevels[i + 1] + '<br>' : '+');
-         }
-         return div;
-};
-
-// Add Legend to the Map
-legend.addTo(myMap);
